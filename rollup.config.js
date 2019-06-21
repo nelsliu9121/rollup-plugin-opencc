09ac62324b6493e31c0d00548e78f3226213efd4
Nelson Liu <nelsliu9121@gmail.com>
2019-06-21 14:40:18 +0800

    🔥 No sourcemap (whatev)

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
-
+import json from "rollup-plugin-json";
+import autoExternal from 'rollup-plugin-auto-external';
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
 export default {
 	input: 'src/index.ts',
-	plugins: [ typescript() ],
+	plugins: [ autoExternal(), json(), typescript() ],
 	output: [
 		{ file: pkg.main, format: 'cjs' },
 		{ file: pkg.module, format: 'es' }
